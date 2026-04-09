a776286d199d1b485d63fff96a7293a5290a6c00
LAJS1022 <luisssajs@gmail.com>
2026-04-08 20:19:22 -0600

    Create runinrender.py

---
+++ part3/runinrender.py
@@ -0,0 +1,12 @@
+import os
+from app import create_app, db
+
+config_name = os.getenv('FLASK_ENV', 'development')
+app = create_app(config_name)
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
